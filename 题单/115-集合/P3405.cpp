71397380a5df557f97d34a0d51b53961c7d6057f
ozline <ozlinex@outlook.com>
2023-01-23 23:49:08 +0800

    230123

--- 题单/115-集合/P3405.cpp
+++ 题单/115-集合/P3405.cpp
@@ -1,7 +1,7 @@
 /**
  * @author OZLIINEX
- * @brief 模板
- * @date 2023-01-07
+ * @brief P3405 [USACO16DEC]Cities and States S
+ * @date 2023-01-23
  */
 #include <iostream>
 #include <cstdio>
@@ -18,9 +18,8 @@
 #include <stack>
 #include <unordered_map>
 
-#define DEBUG
+// #define DEBUG
 
-#define MAXN 500001
 #define INF 0x3f3f3f3f
 #define INFLL 1LL<<60
 #define writespace(a) write(a),putchar(' ')
@@ -58,8 +57,34 @@ inline void write(int x) {
 }
 /* END OF TEMPLATE */
 
+int n, ans;
+map<string, int> mp;
+
+
+ull base = 131, prime = 1e9 + 7;
+ull mod = 212370440130137957ull;
+ull hashe(char *str)
+{
+    ull ans = 0;
+    for(int i = 0; str[i]; i++)
+    {
+        ans = (ans * base + (ll)str[i]) % mod + prime;
+    }
+    return ans;
+}
+
 void solve()
 {
+    string str1, str2;
+    cin >> n;
+    req(i, 1, n)
+    {
+        cin >> str1 >> str2;
+        str1 = str1.substr(0, 2);
+        if(str1 != str2) ans += mp[str1 + str2];
+        mp[str2 + str1]++;
+    }
+    cout << ans << endl;
     return;
 }
 
@@ -69,7 +94,7 @@ int main()
         freopen("in.in","r",stdin); freopen("out.out","w",stdout);
     #endif
 
-    fastio(); solve();
+    solve();
 
     #ifdef DEBUG
         fclose(stdin); fclose(stdout);
