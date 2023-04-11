9ca086619c7bb438a931a4353c634c1f83ef6085
ozline <ozlinex@outlook.com>
2023-04-11 23:50:48 +0800

    feat: add 1363/1807/1100

---
+++ P1017.cpp
@@ -0,0 +1,93 @@
+/**
+ * @author OZLIINEX
+ * @brief P1017 [NOIP2000 提高组] 进制转换
+ * @date 2023-04-11
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <ctime>
+#include <stack>
+#include <unordered_map>
+
+#define MAXN 500001
+#define INF 0x3f3f3f3f
+#define INFLL 1LL<<60
+#define writespace(a) write(a),putchar(' ')
+#define writeln(a) write(a),putchar('\n')
+#define req(a,b,c) for(int (a)=(b);(a)<=(c);(a)++)
+#define fastio() std::ios::sync_with_stdio(false), std::cin.tie(0)
+
+#ifdef DEBUG
+    #define debug(...) fprintf(stdout,__VA_ARGS__)
+    #define debugvar(a) cout<<"[DEBUG] "#a" = "<<a<<endl
+#else
+    #define debug(...) fun()
+    #define debugvar(a) fun()
+    void fun() { return; }
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+
+
+inline int read() {
+    int x = 0, w = 1; char ch = 0;
+    while(ch < '0' || ch > '9') { if(ch == '-') w = -1; ch = getchar(); }
+    while(ch >= '0' && ch <= '9') { x = x * 10 + (ch - '0'); ch = getchar(); }
+    return(x * w);
+}
+
+inline void write(int x) {
+    if(x == 0) { putchar('0'); return; }
+    static int sta[35]; int top = 0;
+    if(x<0) putchar('-'), x = -x;
+    while(x) { sta[top++] = x % 10, x /= 10; }
+    while(top) putchar(sta[--top] + 48);
+}
+/* END OF TEMPLATE */
+
+int n, r;
+int ans[MAXN], cnt;
+
+void solve()
+{
+    cin >> n >> r; cout << n << "=";
+
+    int tr = abs(r); // 让模数永远是正的
+
+    while(n != 0)
+    {
+        if(n < 0 || n % r == 0) ans[++cnt] = -r + n % r, n = n / r;
+        else ans[++cnt] = n % r, n = n / r;
+    }
+
+    // output
+    for(int i = cnt; i > 0; i--) cout << (ans[i] > 9 ? ans[i] - 10 + 'A' : ans[i]);
+
+    cout<< "(base" << r << ")" << endl;
+}
+
+int main()
+{
+    #ifdef DEBUG
+        freopen("in.in","r",stdin); freopen("out.out","w",stdout);
+    #endif
+
+    // fastio();
+    solve();
+
+    #ifdef DEBUG
+        fclose(stdin); fclose(stdout);
+    #endif
+    return 0;
+}
